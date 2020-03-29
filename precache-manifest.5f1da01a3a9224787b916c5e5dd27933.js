049f1f6b8ebdf28d3763f05da27c8c62ccde7746
안쌤/todoapp <mvcweb@naver.com>
2020-03-29 16:11:08 +0900

    Updates

---
+++ precache-manifest.5f1da01a3a9224787b916c5e5dd27933.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e901f3537654890f687c91501b95219",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "12025e92d7331c3f65a4",
+    "url": "/todoapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c13259faf6f8b30c57d9",
+    "url": "/todoapp/static/js/2.57b3c285.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.57b3c285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12025e92d7331c3f65a4",
+    "url": "/todoapp/static/js/main.375b433e.chunk.js"
+  },
+  {
+    "revision": "370f623f98521c72642c",
+    "url": "/todoapp/static/js/runtime-main.7d8159f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
